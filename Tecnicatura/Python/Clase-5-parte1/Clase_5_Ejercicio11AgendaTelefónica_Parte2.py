2d06e1e30df2274bed6c99d2b3930736c9cfa867
Arón Rojas <c1.aronrojas@gmail.com>
2024-11-08 22:10:42 -0300

    Se suma Python

---
+++ Tecnicatura/Python/Clase-5-parte1/Clase_5_Ejercicio11AgendaTelefónica_Parte2.py
@@ -0,0 +1,45 @@
+#Ejercicio 11: Agenda telefonica 
+# Hacer un programa que simule una agenda de contactos. crear un
+# diccionario donde la clave sea el nombre del usuario y el valor 
+# sea el telefono , el programa tendra el siguiente menu de opciones 
+#      1. Nuevo contacto
+#      2. Borrar contacto
+#      3. Ver contactos existentes
+#      4. Salir 
+
+agenda = {}
+while True:
+    print("Menu")
+    print("1_ Nuevo contacto ")
+    print("2_ Borrar contacto")
+    print("3_ Ver contactos existentes ")
+    print("4_ Salir")
+    opcion= int(input("Digite una opcion de menu: "))
+    if opcion ==1:
+        nombre = input("Digite el nombre del contacto:")
+        telefono = input("Digite el numero de telefono")
+        if nombre not in agenda:
+            agenda[nombre] = telefono
+            print("Contacto agregado exitosamente")
+        else:
+           print("Este nombre del contacto ya existe ")
+    elif opcion == 2:
+        nombre = input("Cual es el nombre del contacto: ")
+        if nombre in agenda:
+            del (agenda[nombre])
+            print("Se ha eliminado el contacto correctamente")
+        else:
+            print("Este contacto no existe en la agenda ")
+    elif opcion == 3:
+        print("Agenda de contactos ")
+        for clave, valor in agenda.items():
+            print(f"Nombre: {clave}, Telefono: {valor}")
+    elif opcion == 4:
+        print("Gracias por utilizar su agenda de contactos")
+        break     
+    else:
+        print("Se equivoco de opcion de menu")
+    print()   
+
+
+
